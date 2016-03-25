3b63c6184fece794996e4dd5de4b3e4fe611f921
myriadcoiner <kiah.morante@gmail.com>
2016-03-25 18:51:53 +1100

    Update technology.php
    minor edits

--- web/technology.php
+++ web/technology.php
@@ -14,7 +14,7 @@
 			<p><?php echo translate("5 Algorithms, catering for ASIC, GPU and CPU mining:", $lang); ?>
 			<dl>
 				<dt>SHA256d</dt>
-				<dd class="text-justify"><?php echo translate("Two rounds of the SHA256 hashing algorithm, first used by the pioneer cryptocurrency Bitcoin.", $lang); ?></dd>
+				<dd class="text-justify"><?php echo translate("Two rounds of the SHA256d hashing algorithm, first used by the pioneer cryptocurrency Bitcoin.", $lang); ?></dd>
 				<dt>Scrypt</dt>
 				<dd class="text-justify"><?php echo translate("Designed to be a CPU-only password hashing algorithm, now mined with ASICs on many altcoins, starting with Litecoin.", $lang); ?></dd>
 				<dt>Myr-Groestl</dt>
@@ -22,12 +22,12 @@
 				<dt>Skein</dt>
 				<dd class="text-justify"><?php echo translate("Another  of the five finalists in the NIST SHA-3 hash competition, first used by Skeincoin.", $lang); ?></dd>
 				<dt>Qubit</dt>
-				<dd class="text-justify"><?php echo translate("Originally a CPU only algorithm, Qubit combines 5 competitors in the NIST SHA-3 hash competition - Luffa, Cubehash, SHAvite, SIMD and ECHO.", $lang); ?></dd>
+				<dd class="text-justify"><?php echo translate("Originally a CPU-only algorithm, Qubit combines 5 competitors in the NIST SHA-3 hash competition - Luffa, Cubehash, SHAvite, SIMD and ECHO.", $lang); ?></dd>
 			</ul>
 		</div>
 		<div class="col-lg-4">
 			<h2><?php echo translate("Merge Mining", $lang); ?></h2>
-			<p class="text-justify"><?php echo translate("Since block 4002000, merge mining has been allowed on the Sha256d and Scrypt algorithms. This has enabled Myriad to take advantage of the huge hashrates currently in use
+			<p class="text-justify"><?php echo translate("Since block 4002000, merge mining has been allowed on the SHA256d and Scrypt algorithms. This has enabled Myriad to take advantage of the huge hashrates currently in use
 			by coins using these algorithms, further securing the Myriad blockchain.", $lang); ?></p>
 			<p class="text-justify"><?php echo translate("Merge Mining, or AuxPow, allows the work being done for a parent coin (such as Bitcoin or Litecoin) to be used to find valid blocks on Myriad's blockchain, with no extra load
 			or performance impact to the parent coin.", $lang); ?></p>
@@ -46,13 +46,13 @@
 		<div class="col-lg-12">
 			<h2 class="text-left"><?php echo translate("Myriad's History", $lang); ?></h3>
 			<p class="text-justify"><?php echo translate("Myriad was launched in February 2014 by 8bitcoder, forked from Zetacoin. It was the first cryptocurrency to use five proof-of-work algorithms
-			in an effort to increase security as well as broaden the base of people who could mine it - SHA256 and Scrypt for ASIC miners,
+			in an effort to increase security as well as broaden the base of people who could mine it - SHA256d and Scrypt for ASIC miners,
 			Skein and Myr-Groestl for GPU miners, and Qubit for GPU and CPU miners. Huntercoin, launched in late 2013, was actually the first
-			coin to use multiple PoW algorithms (SHA256 and Scrypt), but its focus was not on mining, rather on a blockchain-based game that produced the large majority of new coins.", $lang); ?></p>
+			coin to use multiple PoW algorithms (SHA256d and Scrypt), but its focus was not on mining, rather on a blockchain-based game that produced the large majority of new coins.", $lang); ?></p>
 			<p class="text-justify"><?php echo translate("Myriad has spawned several spin-offs, the first of which was Saffroncoin, also utilizing five PoW algorithms. 
 			Digitalcoin and Digibyte both hard-forked to use three and five PoW algorithms, respectively. Unitus is another five-algo PoW coin that shares several PoW algos with Myriad,
 			and allows users to merge-mine on all five of its algos. Auroracoin and Verge have also announced plans to fork to multi-algo.", $lang); ?></p>
-			<p class="text-justify"><?php echo translate("In August 2015 Myriad hard-forked to allow merge-mining on its SHA256 and Scrypt algorithms in an effort to improve security and achieve a more stable hashrate on those two algorithms.
+			<p class="text-justify"><?php echo translate("In August 2015 Myriad hard-forked to allow merge-mining on its SHA256d and Scrypt algorithms in an effort to improve security and achieve a more stable hashrate on those two algorithms.
 			At this time the target block time for each algorithm was also changed to five minutes (previously 2.5 minutes), to achieve an overall block target time of 1 minute (previously 30 seconds);
 			this change will stretch the emission curve out by a factor of 2x, to approximately 20 years.", $lang); ?></p>
 		</div>
