494bacfcf537928158d871d0ee74d13c515a1032
myriadcoiner <kiah.morante@gmail.com>
2016-09-12 17:40:55 +1000

    Add Litebit MYR/EUR exchange

--- web/services.php
+++ web/services.php
@@ -19,6 +19,7 @@
 				<li><a href="https://poloniex.com/exchange/btc_myr" target="_blank">Poloniex</a></li>
 				<li><a href="https://bittrex.com/Market/Index?MarketName=BTC-MYR" target="_blank">Bittrex</a></li>
 				<li><a href="https://www.cryptopia.co.nz/Exchange?market=MYR_BTC" target="_blank">Cryptopia</a></li>
+				<li><a href="https://www.litebit.eu/en/buy/myriadcoin" target="_blank">Litebit MYR/EUR</a></li>
 			</ul>
 		</div>
 		<div class="col-lg-3">
