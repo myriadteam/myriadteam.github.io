d35328e898a2269dacf38891125fcbc4a9812167
myriadcoiner <kiah.morante@gmail.com>
2016-09-14 15:41:07 +1000

    Dutch

--- web/include/header.php
+++ web/include/header.php
@@ -12,8 +12,8 @@
 
 <?php
 
-	$languages = array("en","es","eo","zh","el","ru","tl","fr","id","bg","de","it","ar","pl","ko","pt","vn","he");
-	$langnames = array("en" => "English", "es" => "Espanol", "eo" => "Esperanto", "zh" => "Chinese", "el" => "Greek", "ru" => "Russian", "tl" => "Tagalog", "fr" => "French", "id" => "Indonesian", "bg" => "Bulgarian", "de" => "German", "it" => "Italian", "ar" => "Arabic", "pl" => "Polish", "pt" => "Portuguese", "ko" => "Korean", "vn" => "Vietnamese", "he" => "Hebrew");
+	$languages = array("en","es","eo","zh","el","ru","tl","fr","id","bg","de","it","ar","pl","ko","pt","vn","he","nl");
+	$langnames = array("en" => "English", "es" => "Espanol", "eo" => "Esperanto", "zh" => "Chinese", "el" => "Greek", "ru" => "Russian", "tl" => "Tagalog", "fr" => "French", "id" => "Indonesian", "bg" => "Bulgarian", "de" => "German", "it" => "Italian", "ar" => "Arabic", "pl" => "Polish", "pt" => "Portuguese", "ko" => "Korean", "vn" => "Vietnamese", "he" => "Hebrew", "nl" => "Netherlands");
 	$titles = array();
 	$titles['en']['default'] = 'A Coin For Everyone';
 	$titles['en']['technology'] = 'Technology';
@@ -141,6 +141,12 @@
 	$titles['he']['services'] = 'שירותים';
 	$titles['he']['social'] = 'חֶברָתִי';
 
+	$titles['nl']['default'] = 'A Coin For Everyone';
+	$titles['nl']['technology'] = 'Technology';
+	$titles['nl']['wallets'] = 'Wallets';
+	$titles['nl']['mining'] = 'Mining';
+	$titles['nl']['services'] = 'Services';
+	$titles['nl']['social'] = 'Social';
 	
 	switch ($path)
 	{
