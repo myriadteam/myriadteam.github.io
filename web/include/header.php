566afc9bee783ba480fa2aa3aa6fb5297a87290c
myriadcoiner <kiah.morante@gmail.com>
2016-09-14 17:20:33 +1000

    Romanian

--- web/include/header.php
+++ web/include/header.php
@@ -12,8 +12,8 @@
 
 <?php
 
-	$languages = array("en","es","eo","zh","el","ru","tl","fr","id","bg","de","it","ar","pl","ko","pt","vn","he","nl","hr","tr");
-	$langnames = array("en" => "English", "es" => "Espanol", "eo" => "Esperanto", "zh" => "Chinese", "el" => "Greek", "ru" => "Russian", "tl" => "Tagalog", "fr" => "French", "id" => "Indonesian", "bg" => "Bulgarian", "de" => "German", "it" => "Italian", "ar" => "Arabic", "pl" => "Polish", "pt" => "Portuguese", "ko" => "Korean", "vn" => "Vietnamese", "he" => "Hebrew", "nl" => "Netherlands", "hr" => "Croatian", "tr" => "Turkish");
+	$languages = array("en","es","eo","zh","el","ru","tl","fr","id","bg","de","it","ar","pl","ko","pt","vn","he","nl","hr","tr","ro");
+	$langnames = array("en" => "English", "es" => "Espanol", "eo" => "Esperanto", "zh" => "Chinese", "el" => "Greek", "ru" => "Russian", "tl" => "Tagalog", "fr" => "French", "id" => "Indonesian", "bg" => "Bulgarian", "de" => "German", "it" => "Italian", "ar" => "Arabic", "pl" => "Polish", "pt" => "Portuguese", "ko" => "Korean", "vn" => "Vietnamese", "he" => "Hebrew", "nl" => "Netherlands", "hr" => "Croatian", "tr" => "Turkish", "ro" => "Romanian");
 	$titles = array();
 	$titles['en']['default'] = 'A Coin For Everyone';
 	$titles['en']['technology'] = 'Technology';
@@ -160,7 +160,14 @@
 	$titles['tr']['wallets'] = 'Сüzdanlar';
 	$titles['tr']['mining'] = 'Mining';
 	$titles['tr']['services'] = 'Hizmetler';
-	$titles['tr']['social'] = 'Sosyal ağ;
+	$titles['tr']['social'] = 'Sosyal ağ';
+	
+	$titles['ro']['default'] = 'A Coin For Everyone';
+	$titles['ro']['technology'] = 'Technology';
+	$titles['ro']['wallets'] = 'Wallets';
+	$titles['ro']['mining'] = 'Mining';
+	$titles['ro']['services'] = 'Services';
+	$titles['ro']['social'] = 'Social';
 	
 	switch ($path)
 	{
